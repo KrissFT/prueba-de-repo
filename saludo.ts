53939adab358bd13d22a68d58090e2d691f6e445
usuario@PUERTA18.CORP.AR <usuario@PUERTA18.CORP.AR>
2022-05-11 12:22:21 -0300

    first commit

---
+++ saludo.ts
@@ -0,0 +1,39 @@
+class persona {
+    private nacimiento:number;
+    private nombre:string;
+
+    constructor(anioNacimiento:number, nombre:string){
+        this.nacimiento = anioNacimiento;
+        this.nombre = nombre;
+    };
+
+    public cualEsTuNombre():string{
+        return "Mi nombre es " + this.nombre.split(" ");
+    };
+
+    public cualEsTuEdad():string{
+        let edad = 2022 - this.nacimiento;
+        return "Mi edad es " + edad + " años";
+    };     
+    public cualEsTuGeneracion() {
+        if (this.nacimiento < 1975) {
+            return "Soy un Baby Boomer";
+        }
+        if (this.nacimiento >= 1975 && this.nacimiento <= 1999) {
+            return "Soy un Millenial";
+        }
+        if (this.nacimiento > 1999 && this.nacimiento < 2010) {
+            return "Soy un Centenial";
+        }
+        if (this.nacimiento >= 2010) {
+            return "Soy un bebé parlante"
+        }
+    }
+};
+
+let fede = new persona(2010,"Federico Fernandez");
+console.log(fede.cualEsTuNombre());
+console.log(fede.cualEsTuEdad());
+console.log(fede.cualEsTuGeneracion());
+
+
